c3abec1be90c453606d2ad9cff7e3880ad9488b4
Bharat Bethalam <betworkks@gmail.com>
2016-08-08 22:39:28 -0700

    added string project and solved 3Sum problem in n^2, need to optimize it to nlogn

---
+++ src/ArraysAndStrings/3Sum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArraysAndStrings
+{
+    public class _3Sum
+    {
+        public bool find3SumToZero(int[] array)
+        {
+            // nlog(n)
+            Array.Sort(array);
+
+            // n^2 solution
+            // todo : optimize to nlon(n) using binary search
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                var current = array[i];
+
+                var low = i + 1;
+                var high = array.Length - 1;
+
+                while( low < high)
+                {
+                    var sum = current + array[low] + array[high];
+
+                    if (sum == 0)
+                    {
+                        Console.WriteLine("{0}, {1}, {2}", current, array[low], array[high] );
+                        return true;
+                    }          
+
+                    else if (sum > 0)
+                        high--;
+                    else low++;
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
--- src/Strings/Program.cs
+++ src/Strings/Program.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ArraysAndStrings
+namespace Strings
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var threeSum = new _3Sum();
-
-            int[] arr = { -1, 0, 1, 2, -1, -4 };
-            threeSum.find3SumToZero(arr);
-            Console.Read();
         }
     }
 }
