8a87ef7f731509eeed1e2d781c685634cf7b41d3
Bharat Bethalam <betworkks@gmail.com>
2016-08-13 22:58:12 -0700

    added solution for median of two sorted arrays problem

--- src/ArraysAndStrings/Program.cs
+++ src/ArraysAndStrings/Program.cs
@@ -8,16 +8,25 @@ namespace ArraysAndStrings
     public class Program
     {
         public static void Main(string[] args)
-        {            
-            int[] arr = { -1, 0, 1, 2, -1, -4 };
+        {
+            //int[] arr = { -1, 0, 1, 2, -1, -4 };
+            //int[] arr2 = { 5, 3, 7, 2 };
 
             //var threeSum = new _3Sum();
             //threeSum.find3SumToZero(arr);
 
-            var maxSubarray = new MaxSubArray();
-            var max = maxSubarray.FindMaxSubArraySum(arr);
-            Console.WriteLine(max);
+            //var maxSubarray = new MaxSubArray();
+            //var max = maxSubarray.FindMaxSubArraySum(arr);
+            //Console.WriteLine(max);
 
+            //var ms = new MergeSort();
+            //ms.Sort(arr2);
+
+            int[] arr1 = { 1,2,8,9 }; 
+            int[] arr2 = { 3,6,10,11 };
+
+            MedianOfArrays obj = new MedianOfArrays();
+            obj.FindMedian(arr1, arr2);
 
             Console.Read();
         }
