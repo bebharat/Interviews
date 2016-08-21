040b35116deb720879f953bf6c4e14c30234eddf
Bharat Bethalam <betworkks@gmail.com>
2016-08-21 09:47:39 -0700

    merging of two sorted lists

---
+++ src/LinkedList/MergeTwoLinkedLists.cs
@@ -0,0 +1,31 @@
+﻿using DataStructures;
+
+namespace LinkedList
+{
+    public class MergeTwoLinkedLists
+    {
+        public LinkedListNode Merge(LinkedListNode list1, LinkedListNode list2)
+        {
+            LinkedListNode result = null;
+
+            if (list1 == null && list2 == null)
+                return result;
+
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            if (list1.Value < list2.Value)
+            {
+                result = list1;
+                result.Next = Merge(list1.Next, list2);
+            }
+            else
+            {
+                result = list2;
+                result.Next = Merge(list1, list2.Next);
+            }
+
+            return result;
+        }   
+    }
+}
--- src/LinkedList/Program.cs
+++ src/LinkedList/Program.cs
@@ -15,14 +15,24 @@ namespace LinkedList
             n.Next.Next = new LinkedListNode(30);
             n.Next.Next.Next = new LinkedListNode(40);
 
+            LinkedListNode m = new LinkedListNode(5);
+            m.Next = new LinkedListNode(25);
+            m.Next.Next = new LinkedListNode(30);
+            m.Next.Next.Next = new LinkedListNode(37);
+
             //var obj = new ReverseLinkedList();
             //var reverseList = obj.Reverse(n);
 
             //var obj = new ReverseLinkedList();
             //var reverseList = obj.Reverse_Recursively(n);
 
-            var obj = new RemoveKthNode();
-            var afterRemoval = obj.DeleteK(n, 2);
+            //var obj = new RemoveKthNode();
+            //var afterRemoval = obj.DeleteK(n, 2);
+
+            var obj = new MergeTwoLinkedLists();
+            var afterMerge = obj.Merge(n,m);
+
+            Console.ReadLine();
         }
     }
 }
